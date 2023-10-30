9938d31abaa703b90a8d1ebbec9f9755407519a7
GODISGREATAMEN <mixi271020@gmail.com>
2023-10-30 15:27:46 +0200

    Correct class Main

--- src/operatorsPractice/Main.java
+++ src/operatorsPractice/Main.java
@@ -1,22 +1,31 @@
+package operatorsPractice;
+
 public class Main {
     public static void main(String[] args) {
-        int productNum = 1, daysQuantity = 5, productQuantity = 45;
+        int productNum = 1;
+        int daysQuantity = 5;
+        int productQuantity = 45;
         double productPrice = 120.5;
         String productName = "Iphone 6";
+        double totalPrice = productQuantity * productPrice;
+        double pricePerDay = productQuantity * productPrice / daysQuantity;
+
 
         System.out.printf("Product No %d: %s,%ntotal sales for %d days is EUR %.2f,%nsales by day is EUR %.2f."
                 , productNum, productName,
-                daysQuantity, productQuantity * productPrice, productQuantity * productPrice / daysQuantity);
+                daysQuantity, totalPrice, pricePerDay);
 
         productNum = 2;
         daysQuantity = 7;
         productQuantity = 32;
         productPrice = 205.70;
         productName = "Iphone 13";
+        totalPrice = productQuantity * productPrice;
+        pricePerDay = productQuantity * productPrice / daysQuantity;
 
         System.out.printf("%n%nAProduct No %d: %s,%ntotal sales for %d days is EUR %.2f,%nsales by day is EUR %.2f."
                 , productNum, productName,
-                daysQuantity, productQuantity * productPrice, productQuantity * productPrice / daysQuantity);
+                daysQuantity, totalPrice, pricePerDay);
 
 
     }
